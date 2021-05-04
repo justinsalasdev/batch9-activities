f344098bb2f1d4d726dfb2fc69e3e20dd3ea314f
l0ckstreet <rsalas7793@gmail.com>
2021-05-04 22:21:57 +0800

    landing-page: done adding navigation

--- landing-page/scripts/index.js
+++ landing-page/scripts/index.js
@@ -1,3 +1,7 @@
 import startAnimation from "./src/multipleSlide.js";
+import addNavTransition from "./src/navTransition.js";
+import addNavExpansion from "./src/navExpand.js";
 
+addNavExpansion();
+addNavTransition();
 requestAnimationFrame(startAnimation());
---
+++ landing-page/scripts/src/navExpand.js
@@ -0,0 +1,34 @@
+import { navBar } from "./navTransition.js";
+const navAction = navBar.querySelector(".navigation__action");
+const navMenu = navBar.querySelector(".navigation__menu");
+let isOpen = false;
+
+export default function () {
+  navAction.addEventListener("click", function () {
+    toggleMenu();
+    navAction.classList.toggle("navigation__action_shown", isOpen);
+  });
+}
+
+function toggleMenu() {
+  const action = isOpen ? "hide" : "show";
+  animate(navMenu, "navigation__menu", action, isOpen);
+  isOpen = !isOpen;
+}
+
+function animate(target, rootClass, action, hide) {
+  target.classList.remove(`${rootClass}_hidden`); //remove hidden class --> display: none;
+  target.classList.add(`${rootClass}_${action}`);
+  target.addEventListener(
+    "animationend",
+    function endAnimation(event) {
+      target.classList.remove(`${rootClass}_${action}`); // Remove the animation class
+      if (hide) {
+        target.classList.add(`${rootClass}_hidden`);
+        // target.setAttribute("hidden", "true");
+      }
+      target.removeEventListener("animationend", endAnimation, false); // Remove this event listener
+    },
+    false
+  );
+}
