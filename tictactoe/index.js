52773c78b12bc3d63773bd588b10ed730044eb84
l0ckstreet <rsalas7793@gmail.com>
2021-05-09 12:59:22 +0800

    tictactoe: added main game functionality

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -13,16 +13,14 @@ const WINNING_COMBINATIONS = [
 
 const cells = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
+const status = document.getElementById("status");
+const reset = document.getElementById("reset");
 
 let circleTurn = false;
 
-cells.forEach(cell => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
-
 //main ----------------
-//init turn
-board.classList.add("x");
+initGame();
+//-------------------------
 
 function handleClick(e) {
   const target = e.target;
@@ -31,18 +29,76 @@ function handleClick(e) {
   target.classList.add(currentClass);
 
   //checkWin
-  isWin(currentClass);
+  if (isWin(currentClass)) {
+    //update status
+    status.innerText = `${currentClass} wins`;
+    board.classList.add("won");
+
+    //remove event listeners to all cells
+    cells.forEach(cell => cell.removeEventListener("click", handleClick));
+
+    //enable reset button
+    reset.addEventListener("click", initGame);
+    reset.removeAttribute("disabled");
+    return;
+  }
+
+  //checkDraw
+  if (isDraw(currentClass)) {
+    status.innerText = `It's a draw`;
+    board.classList.add("won");
+
+    //remove event listeners to all cells
+    cells.forEach(cell => cell.removeEventListener("click", handleClick));
+
+    //enable reset button
+    reset.addEventListener("click", initGame);
+    reset.removeAttribute("disabled");
+    return;
+  }
 
   //swap turns
   board.classList.toggle(X_CLASS, circleTurn);
   board.classList.toggle(CIRCLE_CLASS, !circleTurn);
   circleTurn = !circleTurn;
+  status.innerText = `${circleTurn ? "circle" : "x"} turn`;
 }
 
 function isWin(currentClass) {
   return WINNING_COMBINATIONS.some(combination => {
-    return combination.every(index => {
+    const isWin = combination.every(index => {
       return cells[index].classList.contains(currentClass);
     });
+    //if winning combination, style winning cells first before returning 'true'
+    if (isWin) {
+      combination.forEach(index => {
+        cells[index].classList.add("won");
+      });
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+function isDraw() {
+  return [...cells].every(cell => {
+    return cell.classList.length > 1; //marked cells have class other than .cell
+  });
+}
+
+function initGame() {
+  const dirtyClasses = ["x", "circle", "won", "draw"];
+  board.classList.remove(...dirtyClasses); //
+  board.classList.add("x"); //init board turn
+  cells.forEach(cell => cell.classList.remove(...dirtyClasses));
+
+  //disable reset
+  reset.removeEventListener("click", initGame);
+  reset.setAttribute("disabled", "true");
+
+  //re-add click handlers
+  cells.forEach(cell => {
+    cell.addEventListener("click", handleClick, { once: true });
   });
 }
