fa6586e4a3bbc03984ca451084d5634bf699719f
l0ckstreet <rsalas7793@gmail.com>
2021-05-11 21:07:00 +0800

    coding-challenge: updated eed-blue team & bouncer

--- js-challenges-and-projects-v.1.2/intro-to-loops/be-the-bouncer!/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-loops/be-the-bouncer!/app.js
@@ -1,26 +1,36 @@
 const nightClubRegister = [
-    {
-        name: 'Tony',
-        lastname: 'Stark',
-        age: 25,
-        gender: 'male'
-    },
-    {
-        name: 'Harry',
-        lastname: 'Potter',
-        age: 16,
-        gender: 'male'
-    },
-    {
-        name: 'Hermione',
-        lastname: 'Granger',
-        age: 17,
-        gender: 'female'
-    },
-    {
-        name: 'Steve',
-        lastname: 'Rogers',
-        age: 62,
-        gender: 'male'
-    }
-]
+  {
+    name: "Tony",
+    lastname: "Stark",
+    age: 25,
+    gender: "male"
+  },
+  {
+    name: "Harry",
+    lastname: "Potter",
+    age: 16,
+    gender: "male"
+  },
+  {
+    name: "Hermione",
+    lastname: "Granger",
+    age: 17,
+    gender: "female"
+  },
+  {
+    name: "Steve",
+    lastname: "Rogers",
+    age: 62,
+    gender: "male"
+  }
+];
+
+nightClubRegister.forEach(person => {
+  const { name, lastname, age, gender } = person;
+  const salutation = `${gender == "male" ? "Mr." : "Ms."}`;
+  if (age < 18) {
+    console.log(`${salutation} ${name} ${lastname}, you are too young for this.`);
+  } else {
+    console.log(`Welcome to heaven, ${salutation} ${name} ${lastname}`);
+  }
+});
