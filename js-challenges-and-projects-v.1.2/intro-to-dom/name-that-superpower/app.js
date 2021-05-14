4bf988598bed1eb93b7327271834b5d833eb782a
l0ckstreet <rsalas7793@gmail.com>
2021-05-14 20:22:30 +0800

    coding-challenge: finished intro-to-dom challenge

--- js-challenges-and-projects-v.1.2/intro-to-dom/name-that-superpower/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-dom/name-that-superpower/app.js
@@ -1,7 +1,15 @@
 var justiceLeague = [
-    {name: "Superman", superpower: "Super strength"}, 
-    {name: "Batman", superpower: "Super rich"},
-    {name: "Wonder Woman", superpower: "Super lasso"}, 
-    {name: "The Flash", superpower: "Super speed"}, 
-    {name: "Green Lantern", superpower: "Super ring"}
-]
+  { name: "Superman", superpower: "Super strength" },
+  { name: "Batman", superpower: "Super rich" },
+  { name: "Wonder Woman", superpower: "Super lasso" },
+  { name: "The Flash", superpower: "Super speed" },
+  { name: "Green Lantern", superpower: "Super ring" }
+];
+
+const list = document.getElementById("league");
+
+justiceLeague.forEach(({ name, superpower }) => {
+  const item = document.createElement("li");
+  item.textContent = `${name} - ${superpower}`;
+  list.appendChild(item);
+});
