8b8f735200198421197cbac299cd145e39852acb
justinsalasdev <justinsalasdev@gmail.com>
2021-05-17 19:18:18 +0800

    momentum: try fix - changes not reflected to github pages

--- momentum/scripts/index.js
+++ momentum/scripts/index.js
@@ -1,33 +1,34 @@
 import randomIcon from "./icons/random.js";
-import { randomButton, toDoList, toDoInput, nameEl, quoteTextEl, quoteAuthorEl } from "./helpers/handles.js";
+import {
+  randomButton,
+  toDoList,
+  toDoInput,
+  nameEl,
+  quoteTextEl,
+  quoteAuthorEl
+} from "./helpers/handles.js";
 import { defaultQuote, toDos } from "./helpers/constants.js";
 import displayToDos from "./helpers/displayToDos.js";
-import addEdit from './helpers/addEdit.js'
-import generateQuote from './helpers/generateQuote.js'
-
-
-
+import addEdit from "./helpers/addEdit.js";
+import generateQuote from "./helpers/generateQuote.js";
 
 // editButton.innerHTML = editIcon;
 randomButton.innerHTML = randomIcon;
 
+nameEl.textContent = localStorage.getItem("name") || "Boss";
+addEdit(nameEl, "name");
 
-nameEl.textContent = localStorage.getItem('name') || 'Boss'
-addEdit(nameEl,'name');
+addEdit(quoteTextEl, "quotation");
+addEdit(quoteAuthorEl, "author");
 
+const localQuote = localStorage.getItem("quotation");
+const localAuthor = localStorage.getItem("author") || "Anonymous";
 
-
-addEdit(quoteTextEl,'quotation')
-addEdit(quoteAuthorEl,'author')
-
-const localQuote = localStorage.getItem('quotation')
-const localAuthor = localStorage.getItem('author') || 'Anonymous'
-
-if(localQuote){
-  quoteTextEl.textContent = localQuote
+if (localQuote) {
+  quoteTextEl.textContent = localQuote;
   quoteAuthorEl.textContent = localAuthor;
 } else {
-  generateQuote()
+  generateQuote();
 }
 
 //initialize toDoList
@@ -49,10 +50,9 @@ toDoInput.addEventListener("keydown", function (e) {
   }
 });
 
-//update toDos state (updated stated not used)
+//update toDos state (updated stated not used)//
 toDoList.addEventListener("change", e => {
   const checkBox = e.target;
   const index = parseInt(checkBox.dataset.index);
   toDos[index].done = checkBox.checked;
 });
-
