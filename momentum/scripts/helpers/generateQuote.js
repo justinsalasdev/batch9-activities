2ecae7a8e500069dea5042b06fb0785eefe72c69
justinsalasdev <justinsalasdev@gmail.com>
2021-05-19 11:48:33 +0800

    momentum: removed custom quote & author after generating random quotes

--- momentum/scripts/helpers/generateQuote.js
+++ momentum/scripts/helpers/generateQuote.js
@@ -6,28 +6,25 @@ const quoteApiUrl = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
 randomButton.addEventListener("click", generateQuote);
 
 export default async function generateQuote() {
-
-  
   try {
-      let response = await fetch(quoteApiUrl);
-      let { quotes } = await response.json();
-      console.log(quotes)
-      const text = quotes[0].text;
-      const author = quotes[0].author;
-  
-      quoteAuthorEl.textContent = author;
-      quoteTextEl.textContent = text;
-      quoteTextEl.dataset.custom = 'false'
+    let response = await fetch(quoteApiUrl);
+    let { quotes } = await response.json();
+    console.log(quotes);
+    const text = quotes[0].text;
+    const author = quotes[0].author;
 
-      localStorage.removeItem('author')
+    quoteAuthorEl.textContent = author;
+    quoteTextEl.textContent = text;
+    quoteTextEl.dataset.custom = "false";
 
+    localStorage.removeItem("author");
+    localStorage.removeItem("quotation");
   } catch (error) {
-    setDefault()
+    setDefault();
   }
 }
 
-
-function setDefault(){
+function setDefault() {
   quoteAuthorEl.textContent = defaultAuthor;
   quoteTextEl.textContent = defaultQuote;
 }
