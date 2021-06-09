d822280c92afbe8e358a838bad7780bba60f5f90
justinsalasdev <justinsalasdev@gmail.com>
2021-06-09 12:49:04 +0800

    slack-clone: added firebase app-check site key

--- slack-clone/src/firebase/firebase.js
+++ slack-clone/src/firebase/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/app-check";
 import createDMId from "../helpers/createDMId";
 
 const firebaseConfig = {
@@ -21,6 +22,11 @@ const db = firebase.firestore();
 // auth.useEmulator("http://localhost:9099");
 // storage.useEmulator("localhost", 9199);
 // db.useEmulator("localhost", 8080);
+
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate("6LfeJiAbAAAAANFed_vONtRsEYeDP1gh-0ldMx8E");
 
 const setTimeStamp = firebase.firestore.FieldValue.serverTimestamp;
 const getId = firebase.firestore.FieldPath.documentId;
